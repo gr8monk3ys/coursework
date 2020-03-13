ba019342e43b496012d3f9ffe84b3d6d297972b5
lorenzo <lorenzosca7@protonmail.ch>
2020-03-13 12:01:14 -0700

    finished lab 5 CSE 100

--- CSE 100/Lab05/lscaturchio.cpp
+++ CSE 100/Lab05/lscaturchio.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
+#include <vector> 
 using namespace std;
 
-int* arr;
-int n = 0;
+int** matrix;
+int colSize;
 
-int findMaxVal(int*A, int n);
-void countingSort(int* A, int n);
-void radixSort(int* A, int n);
+int findMaxVal(int * A, int n);
+void matrixTranspose(int** matrix, int n);
+void countingSort(int * A, int n);
+void radixSort(int * A, int n);
+void printMatrix(int** A, int n);
 
 int main(){
-    cin >> n;
-    arr = new int[n];
-    int A[n];
 
-    for (int i = 0; i < n; i++){
-        cin >> A[i];
-    }  
+    cin >> colSize;
 
-    radixSort(A, n - 1);
-    for(int i = 0; i < n; i++){
-        cout << A[i] << ";";
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < colSize; j++){
+            cin >> matrix[i][j];
+        }
     }
 
-    return 0;
+    matrixTranspose(matrix, colSize);
 
-    delete[] arr;
+    for(int i = 0; i < colSize; i++){
+        for(int j = 0; j < 10; j++){
+            radixSort(matrix[i], colSize);
+        }
+    }
+    
+    matrixTranspose(matrix, colSize);
+    printMatrix(matrix, colSize);
+
+    return 0;
 }
 
 int findMaxVal(int* A, int n){
-    int max = 0;
-    for(int i = 0; i < n; i++){   
+    int max = A[0];
+    for(int i = 1; i < n; i++){   
         if(max <= A[i]){
             max == A[i];
         }
@@ -37,31 +45,49 @@ int findMaxVal(int* A, int n){
     return max;
 }
 
-void countingSort(int*A, int n, int val){
-    int result[n];
-    int count[val];
+void matrixTranspose(int** matrix, int n){
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; i < 10; i++) {
+            int tmp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = tmp;
+        }
+    }
+}
 
-    for(int i = 0; i <= val; i++){
+void countingSort(int* A, int k){
+    int result[k];
+    int count[10];
+
+    for(int i = 0; i < k; i++){
         count[i] = 0;
     }
-    for(int j = 0; j < n; j++){
+    for(int j = 0; j < sizeof(A); j++){
         count[A[j]] += 1;
     }
 
-    for(int i = 1; i < (val + 1); i++){
+    for(int i = 1; i <= k; i++){
         count[i] += count[i - 1];
     }
 
-    for(int j = (n - 1); j >= 0; j--){
+    for(int j = sizeof(A); j > 0; j--){
         result[count[A[j] - 1]] = A[j];
         count[A[j]] -= 1;
     }
 }
 
-void radixSort(int* A, int n){
-    int max = findMaxVal(A, n);
+void radixSort(int* A, int d){
+    for(int i = 0; i < d; i++){
+        int max = findMaxVal(A, d);
+        countingSort(A,  d);
+    }
+}
 
-    for(int val = max/val > 0; val *= 10){
-        countingSort(A, n, val);
+void printMatrix(int** matrix, int n){
+    for(int i = 0; i < colSize; i++){
+        for(int j = 0; j < 10; j++){
+            cout << matrix[i][j] << ";";
+        }
+        cout << "\n";
     }
 }
