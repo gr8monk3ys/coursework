c8c0789965d0a60ab745e277e048adf3b5c78930
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2020-04-01 19:13:44 -0700

    added lab 07 and wordsearch

---
+++ CSE 100/Lab06/lscaturchio.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+bool isRunning = true;
+int hashSize;
+list<int> *hashTable;
+
+int hashFunction(int m){
+    return m % hashSize;
+}
+
+void insert(int key){
+    hashTable[hashFunction(key)].push_front(key);
+}
+
+void _delete(int key){
+    auto iter = hashTable[hashFunction(key)].begin();
+    auto end = hashTable[hashFunction(key)].end();
+
+    int i = 0;
+
+    while(iter != end){
+        if(*iter == key){
+            iter = hashTable[hashFunction(key)].erase(iter);
+            cout << key << ":DELETED" << ";" << "\n";
+            return;
+        }
+        i++;
+        ++iter;
+    }
+    cout << key << ":DELETE_FAILED" << ";" << "\n";
+}
+
+void search(int key){
+
+    auto iter = hashTable[hashFunction(key)].begin();
+    auto end = hashTable[hashFunction(key)].end();
+
+    int i = 0;
+
+    while(iter != end){
+        if(*iter == key){
+            cout << key << ":FOUND_AT" << hashFunction(key) << "," << i << ";" << "\n";
+             return;
+        }
+        i++;
+        ++iter;
+    }
+    cout << key << ":NOT_FOUND" << ";" << "\n";
+}
+
+void output(){
+    for(int i = 0; i < hashSize; i++){
+        int listi = 0;
+        cout << i << ":";
+
+        auto tableVal = hashTable[i];
+        auto iter = hashTable[i].begin();
+        auto end = hashTable[i].end();
+
+        while(iter != end){
+            listi++; 
+            hashTable[i].size() + 1 == listi ? cout << *iter : cout << *iter << "->";
+            ++iter;
+        }
+        cout << ";" << "\n";
+    }
+}
+
+int main(){
+
+    cin >> hashSize;
+    hashTable = new list<int>[hashSize];
+    char call;
+
+    while(isRunning == true){
+        int key;
+        cin >> call;
+
+        if(call == 'i'){
+            cin >> key;
+            insert(key);
+        } else if(call == 'd'){
+            cin >> key;
+            _delete(key);
+        } else if(call == 's'){
+            cin >> key;
+            search(key);
+        } else if(call == 'o'){
+            output();
+        } else if(call == 'e'){
+            isRunning = false;
+            exit(0);
+        }
+    }
+    return 0;
+}
---
+++ CSE 31/Projects/WordSearch/wordsearch.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// DO NOT INCLUDE OTHER LIBRARY!
+
+// Declarations of the two functions you will implement
+// Feel free to declare any helper functions
+void printPuzzle(char** arr, int n);
+void searchPuzzle(char** arr, int n, char** list, int listSize);
+
+// Main function, DO NOT MODIFY!!!	
+int main(int argc, char **argv) {
+    int bSize = 15;	
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <puzzle file name>\n", argv[0]);
+		return 2;
+	}
+    int i, j;
+    FILE *fptr;
+    char **block = (char**)malloc(bSize * sizeof(char*));
+	char **words = (char**)malloc(50 * sizeof(char*));
+
+    // Open file for reading puzzle
+    fptr = fopen(argv[1], "r");
+    if (fptr == NULL) {
+        printf("Cannot Open Puzzle File!\n");
+        return 0;
+    }
+
+	// Read puzzle block into 2D arrays
+    for(i=0; i<bSize; i++){
+        *(block+i) = (char*)malloc(bSize * sizeof(char));
+
+        fscanf(fptr, "%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c\n", *(block+i), *(block+i)+1, *(block+i)+2, *(block+i)+3, *(block+i)+4, *(block+i)+5, *(block+i)+6, *(block+i)+7, *(block+i)+8, *(block+i)+9, *(block+i)+10, *(block+i)+11, *(block+i)+12, *(block+i)+13, *(block+i)+14 );
+    }
+	fclose(fptr);
+
+	// Open file for reading word list
+	fptr = fopen("states.txt", "r");
+	if (fptr == NULL) {
+        printf("Cannot Open Words File!\n");
+        return 0;
+    }
+	
+	// Save words into arrays
+	for(i=0; i<50; i++){
+		*(words+i) = (char*)malloc(20 * sizeof(char));
+		fgets(*(words+i), 20, fptr);		
+	}
+	
+	// Remove newline characters from each word (except for the last word)
+	for(i=0; i<49; i++){
+		*(*(words+i) + strlen(*(words+i))-2) = '\0';	
+	}
+	
+	// Print out word list
+	printf("Printing list of words:\n");
+	for(i=0; i<50; i++){
+		printf("%s\n", *(words + i));		
+	}
+	printf("\n");
+	
+	// Print out original puzzle grid
+    printf("Printing puzzle before search:\n");
+    printPuzzle(block, bSize);
+	printf("\n");
+
+	// Call searchPuzzle to find all words in the puzzle
+	searchPuzzle(block, bSize, words, 50);
+	printf("\n");
+	
+	// Print out final puzzle grid with found words in lower case
+    printf("Printing puzzle after search:\n");
+    printPuzzle(block, bSize);
+	printf("\n");
+	
+    return 0;
+}
+
+void printPuzzle(char** arr, int n){
+	// This function will print out the complete puzzle grid (arr). It must produce the output in the SAME format as the samples in the instructions.
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			printf("%d", *(*(arr + i) + j));
+			if(j == n){
+				printf("\n");
+			}
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void searchPuzzle(char** arr, int n, char** list, int listSize){
+	// This function checks if arr contains words from list. If a word appears in arr, it will print out that word and then convert that word entry in arr into lower case.
+	
+}
