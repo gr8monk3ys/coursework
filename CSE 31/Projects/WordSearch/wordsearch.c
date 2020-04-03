cae4c6f6ec4187345437353edc58abd93448ad48
Lorenzo <lorenzosca7@protonmail.ch>
2020-04-03 02:09:55 -0700

    modified wordsearch

--- CSE 31/Projects/WordSearch/wordsearch.c
+++ CSE 31/Projects/WordSearch/wordsearch.c
@@ -5,93 +5,183 @@
 
 // Declarations of the two functions you will implement
 // Feel free to declare any helper functions
-void printPuzzle(char** arr, int n);
-void searchPuzzle(char** arr, int n, char** list, int listSize);
+void printPuzzle(char **arr, int n);
+void searchPuzzle(char **arr, int n, char **list, int listSize);
 
-// Main function, DO NOT MODIFY!!!	
-int main(int argc, char **argv) {
-    int bSize = 15;	
-	if (argc != 2) {
+// Main function, DO NOT MODIFY!!!
+int main(int argc, char **argv)
+{
+	int bSize = 15;
+	if (argc != 2)
+	{
 		fprintf(stderr, "Usage: %s <puzzle file name>\n", argv[0]);
 		return 2;
 	}
-    int i, j;
-    FILE *fptr;
-    char **block = (char**)malloc(bSize * sizeof(char*));
-	char **words = (char**)malloc(50 * sizeof(char*));
+	int i, j;
+	FILE *fptr;
+	char **block = (char **)malloc(bSize * sizeof(char *));
+	char **wordss = (char **)malloc(50 * sizeof(char *));
 
-    // Open file for reading puzzle
-    fptr = fopen(argv[1], "r");
-    if (fptr == NULL) {
-        printf("Cannot Open Puzzle File!\n");
-        return 0;
-    }
+	// Open file for reading puzzle
+	fptr = fopen(argv[1], "r");
+	if (fptr == NULL)
+	{
+		printf("Cannot Open Puzzle File!\n");
+		return 0;
+	}
 
 	// Read puzzle block into 2D arrays
-    for(i=0; i<bSize; i++){
-        *(block+i) = (char*)malloc(bSize * sizeof(char));
+	for (i = 0; i < bSize; i++)
+	{
+		*(block + i) = (char *)malloc(bSize * sizeof(char));
 
-        fscanf(fptr, "%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c\n", *(block+i), *(block+i)+1, *(block+i)+2, *(block+i)+3, *(block+i)+4, *(block+i)+5, *(block+i)+6, *(block+i)+7, *(block+i)+8, *(block+i)+9, *(block+i)+10, *(block+i)+11, *(block+i)+12, *(block+i)+13, *(block+i)+14 );
-    }
+		fscanf(fptr, "%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c\n", *(block + i), *(block + i) + 1, *(block + i) + 2, *(block + i) + 3, *(block + i) + 4, *(block + i) + 5, *(block + i) + 6, *(block + i) + 7, *(block + i) + 8, *(block + i) + 9, *(block + i) + 10, *(block + i) + 11, *(block + i) + 12, *(block + i) + 13, *(block + i) + 14);
+	}
 	fclose(fptr);
 
-	// Open file for reading word list
+	// Open file for reading words list
 	fptr = fopen("states.txt", "r");
-	if (fptr == NULL) {
-        printf("Cannot Open Words File!\n");
-        return 0;
-    }
-	
-	// Save words into arrays
-	for(i=0; i<50; i++){
-		*(words+i) = (char*)malloc(20 * sizeof(char));
-		fgets(*(words+i), 20, fptr);		
+	if (fptr == NULL)
+	{
+		printf("Cannot Open wordss File!\n");
+		return 0;
 	}
-	
-	// Remove newline characters from each word (except for the last word)
-	for(i=0; i<49; i++){
-		*(*(words+i) + strlen(*(words+i))-2) = '\0';	
+
+	// Save wordss into arrays
+	for (i = 0; i < 50; i++)
+	{
+		*(wordss + i) = (char *)malloc(20 * sizeof(char));
+		fgets(*(wordss + i), 20, fptr);
 	}
-	
-	// Print out word list
-	printf("Printing list of words:\n");
-	for(i=0; i<50; i++){
-		printf("%s\n", *(words + i));		
+
+	// Remove newline characters from each words (except for the last words)
+	for (i = 0; i < 49; i++)
+	{
+		*(*(wordss + i) + strlen(*(wordss + i)) - 2) = '\0';
 	}
-	printf("\n");
-	
-	// Print out original puzzle grid
-    printf("Printing puzzle before search:\n");
-    printPuzzle(block, bSize);
+
+	// Print out words list
+	printf("Printing list of wordss:\n");
+	for (i = 0; i < 50; i++)
+	{
+		printf("%s\n", *(wordss + i));
+	}
 	printf("\n");
 
-	// Call searchPuzzle to find all words in the puzzle
-	searchPuzzle(block, bSize, words, 50);
+	// Print out original puzzle grid
+	printf("Printing puzzle before search:\n");
+	printPuzzle(block, bSize);
 	printf("\n");
-	
-	// Print out final puzzle grid with found words in lower case
-    printf("Printing puzzle after search:\n");
-    printPuzzle(block, bSize);
+
+	// Call searchPuzzle to find all wordss in the puzzle
+	searchPuzzle(block, bSize, wordss, 50);
 	printf("\n");
-	
-    return 0;
+
+	// Print out final puzzle grid with found wordss in lower case
+	printf("Printing puzzle after search:\n");
+	printPuzzle(block, bSize);
+	printf("\n");
+
+	return 0;
 }
 
-void printPuzzle(char** arr, int n){
+void printPuzzle(char **arr, int n)
+{
 	// This function will print out the complete puzzle grid (arr). It must produce the output in the SAME format as the samples in the instructions.
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			printf("%d", *(*(arr + i) + j));
-			if(j == n){
-				printf("\n");
-			}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%c ", *(*(arr + i) + j));
 		}
 		printf("\n");
 	}
-	printf("\n");
 }
 
-void searchPuzzle(char** arr, int n, char** list, int listSize){
-	// This function checks if arr contains words from list. If a word appears in arr, it will print out that word and then convert that word entry in arr into lower case.
-	
+void searchPuzzle(char **arr, int n, char **list, int listSize)
+{
+	char **foundList = (char **)malloc(n * sizeof(char *));
+
+	for (int words = 0; words < listSize; words++)
+	{
+		for (int letter = 0; letter < strlen(*(list + words)); letter++)
+		{
+			char *word = (char *)malloc((n + 1) * sizeof(char));
+			*(word + letter) = (*(*(list + words) + letter));
+			printf("%s", word);
+
+			char *tempStr = (char *)malloc((n + 1) * sizeof(char));
+
+			for (int row = 0; row < n; row++)
+			{
+				*(foundList + row) = (char *)malloc(n * sizeof(char));
+				for (int col = 0; col < n; col++)
+				{
+					// char* word = "FLORIDA";
+
+					// if (strstr(*(arr + row), word) != 0) {
+					// 		// printf("OMG %s\n", *(arr + row));
+					// 	}
+
+					if ((*(*(list + words) + letter)) >= 97)
+					{
+
+						char tempChar = (char)malloc((1) * sizeof(char));
+						tempChar = (*(*(list + words) + letter + 1));
+						tempChar = (char)(tempChar - 32);
+						printf("%c", tempChar);	
+						*(tempStr + letter) = *(*((arr + row) + col));
+
+						if (*(*(arr + row) + col) == tempChar)
+						{
+
+							if(*(*(list + words) + letter) ==  *(*(arr + row - 1) + col)){
+								*(tempStr + letter + 1) = *(*(arr + row + 1) + col);
+								tempChar = (*(*(list + row + 1) + col));
+								tempChar = (char)(tempChar - 32);
+							} else if(){
+
+							}
+
+							int directionType = 0;
+							switch (directionType)
+							{
+							case 0: // down
+								// searchPuzzle(((arr + row + 1) + col), n, ((list + words) + letter + 1), listSize);
+								// tempStr += *(*(list + words) + letter + 1);
+								*(tempStr + letter + 1) = *(*(arr + row + 1) + col);
+								tempChar = (*(*(list + row + 1) + col));
+								tempChar = (char)(tempChar - 32);
+								break;
+							case 1: // left
+								searchPuzzle(((arr + row) + col - 1), n, ((list + words) + letter + 1), listSize);
+								tempStr += *(*(list + words) + letter + 1);
+								break;
+							case 2: // right
+								searchPuzzle(((arr + row) + col + 1), n, ((list + words) + letter + 1), listSize);
+								tempStr += *(*(list + words) + letter + 1);
+								break;
+							case 3: // down left
+								searchPuzzle(((arr + row + 1) + col - 1), n, ((list + words) + letter + 1), listSize);
+								tempStr += *(*(list + words) + letter + 1);
+								break;
+							case 4: // down right
+								searchPuzzle(((arr + row + 1) + col + 1), n, ((list + words) + letter + 1), listSize);
+								tempStr += *(*(list + words) + letter + 1);
+								break;
+							}
+							directionType++;
+							tempChar = (*(*(list + row) + col));
+							tempChar = (char)(tempChar - 32);
+
+							if (*(*(list + words) + letter) == 0)
+							{
+								*(foundList + words) = tempStr;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
