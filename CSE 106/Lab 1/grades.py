cf511a5630b25a1f6f5ff5740ae4fea2b348373f
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-09-21 03:55:01 -0700

    Finished grading script

---
+++ CSE 106/Lab 1/grades.py
@@ -0,0 +1,34 @@
+def make_student(students):
+    name = input('Enter student name: ')
+    grade = input('Enter student grade: ')
+    students[name] = grade
+    print("Student made: {}, Grade: {}".format(name, grade))
+    print(students)
+    return students
+
+def edit_grade(students):
+    name = input('Enter student name: ')
+    grade = input('Enter new grade: ')
+    students[name] = grade
+    print('New grade is {}'.format(grade))
+    print(students)
+    return students
+
+
+def delete_grade(students):
+    name = input('Enter student name: ')
+    students[name] = ''
+    print('Grade has been deleted.')
+    print(students)
+    return students
+
+
+with open('grades.txt', 'r') as file:
+    students = {}
+    for line in file:
+        entry = line.replace('\"', '').replace(',', '').split(':')
+        students[entry[0].strip("' ")] = entry[1].strip()
+
+    make_student(students)
+    edit_grade(students)
+    delete_grade(students)
