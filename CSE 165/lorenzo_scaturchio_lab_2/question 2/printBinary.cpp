620f36bcb737b900ce4c07060336df65eff8e99d
gr8monk3ys <lorenzosca7@protonmail.ch>
2021-09-23 14:44:00 -0700

    finished question 2

--- CSE 165/lorenzo_scaturchio_lab_2/question 2/printBinary.cpp
+++ CSE 165/lorenzo_scaturchio_lab_2/question 2/printBinary.cpp
@@ -2,6 +2,25 @@
 #include <climits>
 using namespace std;
 
-void printBinary(unsigned char val) {
-    for (int i = CHAR_BIT - 1; i >= 0; i--)
-        cout << "01"[!!(val & (1 << i))];
+int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+void printBinary(void* arr, int size, int val) {
+    int i = 0;
+    while (i < size) {
+        *((int*)arr + i) = val;
+        i++;
+    }
+}
+int main() {
+    const int size = sizeof arr / sizeof * arr;
+    void* p = static_cast<void*>(arr);
+
+    printBinary(p, size, 10);
+
+    int i = 0;
+    while (i < size) {
+        cout << arr[i] << endl;
+        i++;
+    }
+    return 0;
+}
