475ca0a9bb85ce2ce8061b4e84397e5a43998327
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-01 01:19:49 -0700

    finished question 1

--- CSE 111/Lab 4/test/1.sql
+++ CSE 111/Lab 4/test/1.sql
@@ -1,2 +1,5 @@
--- select c_name, o_totalprice from customer, orders, nation
--- where n_name is 'FRANCE' and o_orderdate between '1996-00-00' and '1997-00-00';
+select customer.c_name, SUM(orders.o_totalprice) from orders
+inner join nation on nation.n_nationkey is customer.c_nationkey
+inner join customer on customer.c_custkey is orders.o_custkey
+where nation.n_name is 'FRANCE' and strftime('%Y', o_orderdate) is '1995'
+group by customer.c_name;
