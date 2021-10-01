3f0ac73ed25aa81c1f4cba007f576326921fb5e9
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-01 05:37:01 -0700

    finished question 5

--- CSE 111/Lab 4/test/5.sql
+++ CSE 111/Lab 4/test/5.sql
@@ -1,3 +1,6 @@
-select c_name, count(orders) from customer, orders, nation
-where n_name is 'GERMANY' and o_orderdate between '1993-00-00' and '1994-00-00'
-order by c_name; 
+select c_name, count(o_orderkey) from nation
+inner join orders on o_custkey is c_custkey
+inner join customer on c_nationkey is n_nationkey
+where n_name is 'GERMANY' and strftime('%Y', o_orderdate) is '1993'
+group by c_name
+order by c_name, count(o_orderkey);
