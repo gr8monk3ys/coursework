b088515c00f4aed463842f79219c3860bb6f3bed
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-01 05:36:23 -0700

    finished question 2

--- CSE 111/Lab 4/test/2.sql
+++ CSE 111/Lab 4/test/2.sql
@@ -1,2 +1,4 @@
-select r_name, count(s_name) from supplier, region
+select r_name, count(s_suppkey) from region
+inner join nation on n_regionkey is r_regionkey
+inner join supplier on s_nationkey is n_nationkey
 group by r_name;
