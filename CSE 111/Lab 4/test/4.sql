c98c14028fe927c676a9828968f3c39936309743
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-01 05:36:46 -0700

    finished question  4

--- CSE 111/Lab 4/test/4.sql
+++ CSE 111/Lab 4/test/4.sql
@@ -1,3 +1,6 @@
-select s_name, count(p_size) from supplier, part, nation
+select s_name, count(p_partkey) from nation
+inner join part on p_partkey is ps_partkey
+inner join partsupp on ps_suppkey is s_suppkey
+inner join supplier on s_nationkey is n_nationkey
 where p_size < 20 and n_name is 'CANADA'
 group by s_name;
