417d9d7e470054d623ba53bca349b57095228b1b
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-01 05:37:12 -0700

    finished question 7

--- CSE 111/Lab 4/test/7.sql
+++ CSE 111/Lab 4/test/7.sql
@@ -1,1 +1,6 @@
-select n_name, o_orderstatus from nation, orders
+select n_name, o_orderstatus, count(o_orderkey) from region
+inner join nation on n_regionkey is r_regionkey
+inner join customer on c_nationkey is n_nationkey
+inner join orders on o_custkey is c_custkey
+where r_name is 'AMERICA'
+group by n_name, o_orderstatus;
