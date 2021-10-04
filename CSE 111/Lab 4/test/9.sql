a298f9bd4a254b0932ac33a31206503af326ecdf
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-03 23:54:00 -0700

    finished question 9

--- CSE 111/Lab 4/test/9.sql
+++ CSE 111/Lab 4/test/9.sql
@@ -1,3 +1,5 @@
-select count(distinct o_clerk) from orders, nation
+select count(distinct o_clerk) from nation
+inner join orders on o_orderkey is l_orderkey
+inner join lineitem on l_suppkey is s_suppkey
 inner join supplier on s_nationkey is n_nationkey
-where n_name is 'UNITED STATES';
+where n_name is 'UNITED STATES'
