8865b32070f3e6469d0589f67e36ea349d383820
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-03 23:53:53 -0700

    finished question 8

--- CSE 111/Lab 4/test/8.sql
+++ CSE 111/Lab 4/test/8.sql
@@ -1,4 +1,7 @@
-select n_name, count(distinct o_orderkey) from orders, supplier
-inner join nation on n_nationkey is s_nationkey
-where strftime('%Y', o_orderdate) is '1995' and o_orderstatus is 'F'
-group by n_name;
+select n_name, Y from(select n_name, count(distinct l_orderkey) as Y from region
+inner join supplier on s_nationkey is n_nationkey
+inner join nation on n_regionkey is r_regionkey
+inner join lineitem on l_suppkey is s_suppkey
+inner join orders on o_orderkey is l_orderkey
+where o_orderstatus is 'F' and o_orderdate between '1995-01-01' and '1995-12-31'
+group by n_name) where Y > 50;
