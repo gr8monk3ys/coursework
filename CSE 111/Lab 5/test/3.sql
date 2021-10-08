65c49997c7547a5da8baab5e82b7551ccdcedd30
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-07 17:47:41 -0700

    finished question 3

--- CSE 111/Lab 5/test/3.sql
+++ CSE 111/Lab 5/test/3.sql
@@ -1,3 +1,5 @@
 select min(l_discount) from lineitem
-where o_orderdate between '09-01-1996' and '09-31-1996' 
-and l_discount > (select avg(l_discount) from lineitem);
+inner join orders on o_orderkey is l_orderkey
+where(substr(o_orderdate, 1, 7) is '1996-10' and l_discount > 
+(select avg(l_discount) from lineitem
+inner join orders on o_orderkey is l_orderkey));
