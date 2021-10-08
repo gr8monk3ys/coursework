b1c24ce5329e64329d59ed0207f99f26635642dc
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-07 17:47:56 -0700

    finished question 5

--- CSE 111/Lab 5/test/5.sql
+++ CSE 111/Lab 5/test/5.sql
@@ -1,3 +1,8 @@
-select s_name, p_size, min(ps_supplycost) from supplier, partsupplier, part
-inner join supplier on s_
-where p_type is 'STEEL' and r_name is 'ASIA'
+select s_name, p_size, min(ps_supplycost) from supplier
+inner join partsupp on ps_suppkey is s_suppkey
+inner join nation on s_nationkey is n_nationkey
+inner join region on n_regionkey is r_regionkey
+inner join part on ps_partkey is p_partkey
+where r_name is 'ASIA' and p_type like '%STEEL%'
+group by p_size
+order by s_name, p_size;
