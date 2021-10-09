2b35e5be474b4bf61d6acc3e37ba30a688ae382f
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-08 20:10:30 -0700

    finished quiz 2

--- CSE 111/quiz 2/quiz-2.sql
+++ CSE 111/quiz 2/quiz-2.sql
@@ -4,7 +4,7 @@ inner join Laptop on Laptop.model is Product.model
 where screen > 16 and price < 2000;
 
 --2
-select distinct maker as Maker from Product
+select distinct maker from Product
 where type is 'pc' and maker not in (select maker from Product 
 where type is 'laptop');
 
@@ -22,13 +22,11 @@ group by hd having (count(model) >= 2);
 
 --5
 select model, price from PC
-where price < (select min(price) from Laptop);
+where price < (select max(price) from Laptop);
 
 --6
-
-select maker, PC_count, Printer_count from
-(select maker, (select count(Product.model) from Product 
-where (type = 'pc' or  type = 'laptop') and P1.maker = Product.maker) as  PC_count,
+select maker from (select maker, (select count(Product.model) from Product 
+where (type = 'laptop') and P1.maker = Product.maker) as  Laptop_count,
 (select count(Product.model) from Product 
 where type = 'printer' and P1.maker = Product.maker) as Printer_count from Product as P1
-group by maker) as tb1 where PC_count >= 1 and Printer_count >= 2;
+group by maker) where Laptop_count >= 1 and Printer_count >= 2;
