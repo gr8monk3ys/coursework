36e987765cfbfcea32ef3f72ada824e945dbeb67
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-10 21:03:26 -0700

    finished question 8

--- CSE 111/Lab 5/test/8.sql
+++ CSE 111/Lab 5/test/8.sql
@@ -1,2 +1,4 @@
-select count(distinct s_name) from supplier, part
+select count(distinct s_name) from supplier
+inner join partsupp on s_suppkey is ps_suppkey
+inner join part on p_partkey is ps_partkey
 where p_type like '%POLISHED%' and p_size in (3, 23, 36, 49);
