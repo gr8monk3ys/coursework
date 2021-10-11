bb2f543785c081bc7af9cac9187ce50717616c22
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-10 21:06:48 -0700

    finished question 11

--- CSE 111/Lab 5/test/11.sql
+++ CSE 111/Lab 5/test/11.sql
@@ -1,3 +1,10 @@
-select (l_extendedprice * (1 - l_discount)) from lineitem
--- inner join orders on o_
--- where o_orderdate
+select disctinct p1.p_name from lineitem, orders, part as p1,
+(
+    select p2.p_partkey, min(l_extendedprice*(1-l_discount)) as lowvalue
+    from lineitem, orders,part as p2
+    where l_orderkey is o_orderkey and l_partkey is p2.p_partkey
+    and strftime('%Y-%m-%d', o_orderdate) > '1996-10-02'
+)as minvalueitem
+where l_orderkey is o_orderkey and l_partkey is p1.p_partkey
+and strftime('%Y-%m-%d', o_orderdate) > '1996-10-02'
+and p1.p_partkey is minvalueitem.p_partkey;
