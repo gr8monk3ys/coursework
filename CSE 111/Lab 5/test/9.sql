abbb0c88f348ae6b27ecbd8a9d78da08d702ab3a
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-10 21:04:16 -0700

    finished quesiton 9

--- CSE 111/Lab 5/test/9.sql
+++ CSE 111/Lab 5/test/9.sql
@@ -1,9 +1,9 @@
--- select p_name from part, nation
--- inner join partsupp on ps_partkey is p_partkey
--- where n_name is 'UNITED STATES'
--- group by p_name
--- limit(ps_supplycost * ps_availqty);
-
-
-select (ps_supplycost * ps_availqty) as total from partsupp
-limit 1;
+select p_name 
+from part, supplier, nation, partsupp
+where p_partkey is ps_partkey and ps_suppkey is s_suppkey
+and s_nationkey is n_nationkey and n_nameis 'UNITED STATES'
+order by (ps_supplycost * ps_availqty) desc
+limit (select count(distinct ps_partkey)/100
+    from part, partsupp, supplier, nation
+    where p_partkey is ps_partkey and ps_suppkey is s_suppkey
+    and s_nationkey is n_nationkey and n_nameis 'UNITED STATES');
