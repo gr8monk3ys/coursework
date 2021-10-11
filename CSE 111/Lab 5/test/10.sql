f9c0ffb4e28d28996d7d229540f4b8604b1e6348
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-10 21:05:54 -0700

    finished question 10

--- CSE 111/Lab 5/test/10.sql
+++ CSE 111/Lab 5/test/10.sql
@@ -1,8 +1,6 @@
-select r_name, count(distinct c_custkey) from customer
+select r_name as regions, count(DisTinCT c_name) as count
+from customer orders
 inner join nation on n_nationkey is c_nationkey
 inner join region on r_regionkey is n_regionkey
-inner join orders on o_custkey is c_custkey
-where c_acctbal < (select avg(c_acctbal) from customer)
--- and c_custkey in (select o_custkey from orders)
+where c_acctbal < (select avg(c_acctbal) from customer) and not c_custkey in (select o_custkey from orders)
 group by r_name;
-
