9cc70a7da3ce8154e0ad5a4b0b381696ff2e1875
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-10 21:11:58 -0700

    revised question 9

--- CSE 111/Lab 5/test/9.sql
+++ CSE 111/Lab 5/test/9.sql
@@ -1,9 +1,9 @@
 select p_name 
 from part, supplier, nation, partsupp
 where p_partkey is ps_partkey and ps_suppkey is s_suppkey
-and s_nationkey is n_nationkey and n_nameis 'UNITED STATES'
+and s_nationkey is n_nationkey and n_name is 'UNITED STATES'
 order by (ps_supplycost * ps_availqty) desc
 limit (select count(distinct ps_partkey)/100
     from part, partsupp, supplier, nation
     where p_partkey is ps_partkey and ps_suppkey is s_suppkey
-    and s_nationkey is n_nationkey and n_nameis 'UNITED STATES');
+    and s_nationkey is n_nationkey and n_name is 'UNITED STATES');
