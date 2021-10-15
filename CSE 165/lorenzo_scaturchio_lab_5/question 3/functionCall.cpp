49aed49c1f96b32e57398365016a00446e7aca13
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-14 23:02:29 -0700

    finished question 3

--- CSE 165/lorenzo_scaturchio_lab_5/question 3/functionCall.cpp
+++ CSE 165/lorenzo_scaturchio_lab_5/question 3/functionCall.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Parent {
 public:
-    virtual void printClass() {
+    void printClass() {
         cout << "Parent class" << endl;
     }
 };
@@ -21,23 +21,25 @@ int main() {
     Parent* parent = &child;
     clock_t virtual_function_start, virtual_function_end, non_virtual_function_start, non_virtual_function_end;
 
+    cout << "Non-virtual function execution: \n";
+    non_virtual_function_start = clock();
+    size_t j = 0;
+    while (j < 5000) {
+        parent->printClass();
+        j++;
+    }
+    non_virtual_function_end = clock();
+
     cout << "virtual function execution: \n";
     virtual_function_start = clock();
     size_t i = 0;
-    while (i < 2000) {
+    while (i < 5000) {
         child.printClass();
         i++;
     }
     virtual_function_end = clock();
 
-    cout << "Non-virtual function execution: \n";
-    non_virtual_function_start = clock();
-    size_t j = 0;
-    while (j < 2000) {
-        parent->printClass();
-        j++;
-    }
-    non_virtual_function_end = clock();
+
 
     cout << "Virtual function execution time: " << (double)(virtual_function_end - virtual_function_start) / CLOCKS_PER_SEC << endl;
     cout << "Non-virtual function execution time: " << (double)(non_virtual_function_end - non_virtual_function_start) / CLOCKS_PER_SEC << endl;
