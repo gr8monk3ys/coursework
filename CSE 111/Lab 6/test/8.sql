fc3800948ca90dab3c359dcf4fb902e048e59693
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-17 23:08:22 -0700

    finished question 8

--- CSE 111/Lab 6/test/8.sql
+++ CSE 111/Lab 6/test/8.sql
@@ -1,7 +1,8 @@
-select distinct c_name from region, orders
-inner join supplier on s_nationkey is n_nationkey
-inner join nation on n_regionkey is r_regionkey
+select count(distinct c_name) from orders
 inner join customer on c_custkey is o_custkey
-where r_name is 'AMERICA'
-group by c_name
-having count(o_orderkey) >= 1;
+where o_orderkey not in (select distinct(o_orderkey) from lineitem 
+inner join region on r_regionkey is n_regionkey
+inner join nation on n_nationkey is s_nationkey
+inner join supplier on s_suppkey is l_suppkey
+inner join orders on o_orderkey is l_orderkey
+where r_name NOT in ('AMERICA'));
