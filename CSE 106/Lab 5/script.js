5cb7f0fa8aba9e96b0e86b0c038da3bfd8d4c157
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-26 01:07:45 -0700

    loop logic changed

--- CSE 106/Lab 5/script.js
+++ CSE 106/Lab 5/script.js
@@ -1,28 +1,6 @@
 var API = "https://amhep.pythonanywhere.com";
 
-function get() {
-    let name = document.getElementById("name").value;
-    let request = new XMLHttpRequest();
-    let flag = false;
-
-    request.open("GET", API + "/grades");
-    request.send();
-    request.onload = () => {
-        let data = JSON.parse(request.response);
-        let keys = Object.keys(data);
-        for (let i = 0; i < keys.length; i++) {
-            if (keys[i] == name) {
-                document.getElementById("get_grade").value = data[keys[i]];
-                flag = true;
-            }
-        }
-        if (flag == false) {
-            document.getElementById("get_grade").value = "Name not found";
-        }
-    }
-}
-
-function find() {
+function search() {
     let request = new XMLHttpRequest();
     let text = "<table border='1'><tr><th>Name</th><th>Grade</th></tr>";
     request.open("GET", API + "/grades");
@@ -30,28 +8,40 @@ function find() {
     request.onload = () => {
         let data = JSON.parse(request.response);
         let keys = Object.keys(data);
-        for (let i = 0; i < keys.length; i++) {
+
+        let i = 0;
+        while (i < keys.length) {
             text += "<tr><td>" + keys[i] + "</td><td>" + data[keys[i]] + "</td></tr>";
+            i++;
         }
         text += "</table>";
-        document.getElementById("get_all").innerHTML = text;
+        document.getElementById("getAll").innerHTML = text;
     }
 }
 
-function add() {
-    let name = document.getElementById("name2").value;
-    let grade = document.getElementById("grade2").value;
-    let param =
-    {
-        "name": name,
-        "grade": grade
-    };
+function get() {
+    let name = document.getElementById("name").value;
     let request = new XMLHttpRequest();
-    request.open("POST", API + "/grades", true);
-    request.setRequestHeader("Content-Type", "application/json");
-    let json_data = JSON.stringify(param);
-    request.send(json_data);
-    document.getElementById("success_record").innerHTML = "Record added successfully!";
+    let ping = false;
+
+    request.open("GET", API + "/grades");
+    request.send();
+    request.onload = () => {
+        let data = JSON.parse(request.response);
+        let keys = Object.keys(data);
+
+        let i = 0;
+        while (i < keys.length) {
+            if (keys[i] == name) {
+                document.getElementById("searchGrade").value = data[keys[i]];
+                ping = true;
+            }
+            i++;
+        }
+        if (ping == false) {
+            document.getElementById("searchGrade").value = "Name could not be found";
+        }
+    }
 }
 
 function edit() {
@@ -65,19 +55,35 @@ function edit() {
 
     request.open("PUT", API + "/grades" + "/" + name, true);
     request.setRequestHeader("Content-Type", "application/json");
-    
+
     let json_data = JSON.stringify(param);
     request.send(json_data);
-    document.getElementById("success_delete").innerHTML = "Grade edited successfully!";
+    document.getElementById("delete").innerHTML = "Grade edited";
 }
 
-function del() {
+function includeStudent() {
+    let name = document.getElementById("name2").value;
+    let grade = document.getElementById("grade2").value;
+    let param =
+    {
+        "name": name,
+        "grade": grade
+    };
+    let request = new XMLHttpRequest();
+    request.open("POST", API + "/grades", true);
+    request.setRequestHeader("Content-Type", "application/json");
+    let json_data = JSON.stringify(param);
+    request.send(json_data);
+    document.getElementById("record").innerHTML = "Record includeStudented";
+}
+
+function deleteStudent() {
     let name = document.getElementById("name4").value;
-    
+
     let request = new XMLHttpRequest();
     request.open("DELETE", API + "/grades" + "/" + name, true);
     request.setRequestHeader("Content-Type", "application/json");
     request.send();
-    
-    document.getElementById("success_delete").innerHTML = "Record deleted successfully!";
+
+    document.getElementById("delete").innerHTML = "Record deleted";
 }
