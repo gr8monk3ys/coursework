6885778eac537991a225560440facad5d95aa733
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-10-28 13:13:47 -0700

    finished question 1

---
+++ CSE 165/lorenzo_scaturchio_lab_6/question 1/counted.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Counted {
+public:
+    Counted() : id(count++) { cout << "Counted created, id: " << id << endl; }
+    ~Counted() { cout << "Counted deleted, id: " << id << endl; }
+private:
+    int id;
+    static int count;
+};
+int Counted::count = 0;
+
+int main() {
+    Counted* countPtr = new Counted;
+    delete countPtr;
+    return 0;
+}
