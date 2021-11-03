b96f06d89b55df96cf6dbfb99b7c754f9bfd0b00
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-11-02 22:07:17 -0700

    started question 3

---
+++ CSE 165/lorenzo_scaturchio_lab_7/question 3/arrInt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+class ArrHold {
+static int var = 5;
+static const int[] arr = [var];
+
+ArrHold() {const int }
+
+print() {cout << arr << endl;}
+
+};
+
+int main() {
+
+ArrHold arr;
+
+}
