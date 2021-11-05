df307fe52941a24fddd951c2a6677b07fb85b482
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-11-04 20:32:33 -0700

    finished question 3

--- CSE 165/lorenzo_scaturchio_lab_7/question 3/arrInt.cpp
+++ CSE 165/lorenzo_scaturchio_lab_7/question 3/arrInt.cpp
@@ -4,13 +4,13 @@ using namespace std;
 class ArrHold {
     static const int size = 6;
     const int val;
-    static int number;
+    static int num;
     int arr[size] = {0,1,2,3,4,5};
 public:
     ArrHold() : val(20) {}
 
     static void printNum() {
-        cout << "static int: " << number << endl;
+        cout << "static int: " << num << endl;
     }
 
     inline void printArr() {
@@ -25,7 +25,7 @@ public:
     }
 };
 
-int ArrHold::number = 12;
+int ArrHold::num = 12;
 
 int main() {
     ArrHold a;
