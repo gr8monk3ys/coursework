77c49cf43b6b52d74b13f0845bf659df481e5fbd
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-11-16 21:41:04 -0800

    formatted login file

--- CSE 106/Lab 8/static/js/login.js
+++ CSE 106/Lab 8/static/js/login.js
@@ -3,14 +3,14 @@ const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
-        alert("You logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+  if (username === "user" && password === "web_dev") {
+    alert("You logged in.");
+    location.reload();
+  } else {
+    loginErrorMsg.style.opacity = 1;
+  }
+});
