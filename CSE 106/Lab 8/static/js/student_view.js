36cc480c2646743412e990961ca929bb1c7bfd01
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-11-16 21:41:22 -0800

    implemented table functionality

--- CSE 106/Lab 8/static/js/student_view.js
+++ CSE 106/Lab 8/static/js/student_view.js
@@ -1,15 +1,58 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const tabs = document.getElementsByClassName('tab');
-    for(let i = 0; i < tabs.length; i++) {
-      tabs[i].addEventListener('click', tabSwitch, false);
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const tabs = document.getElementsByClassName("tab");
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].addEventListener("click", tabSwitch, false);
     }
-  
-    function tabSwitch(){
-      document.getElementsByClassName('is-active')[0].classList.remove('is-active');
-      this.classList.add('is-active');
-      document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+
+    function tabSwitch() {
+      document
+        .getElementsByClassName("is-active")[0]
+        .classList.remove("is-active");
+      this.classList.add("is-active");
+      document.getElementsByClassName("is-show")[0].classList.remove("is-show");
       const arrayTabs = Array.prototype.slice.call(tabs);
       const index = arrayTabs.indexOf(this);
-      document.getElementsByClassName('panel')[index].classList.add('is-show');
-    };
-  }, false);
+      document.getElementsByClassName("panel")[index].classList.add("is-show");
+    }
+  },
+  false
+);
+
+$(document).ready(function () {
+  $(".search").keyup(function () {
+    var searchTerm = $(".search").val();
+    var listItem = $(".results tbody").children("tr");
+    var searchSplit = searchTerm.replace(/ /g, "'):containsi('");
+
+    $.extend($.expr[":"], {
+      containsi: function (elem, i, match, array) {
+        return (
+          (elem.textContent || elem.innerText || "")
+            .toLowerCase()
+            .indexOf((match[3] || "").toLowerCase()) >= 0
+        );
+      },
+    });
+
+    $(".results tbody tr")
+      .not(":containsi('" + searchSplit + "')")
+      .each(function (e) {
+        $(this).attr("visible", "false");
+      });
+
+    $(".results tbody tr:containsi('" + searchSplit + "')").each(function (e) {
+      $(this).attr("visible", "true");
+    });
+
+    var jobCount = $('.results tbody tr[visible="true"]').length;
+    $(".counter").text(jobCount + " item");
+
+    if (jobCount == "0") {
+      $(".no-result").show();
+    } else {
+      $(".no-result").hide();
+    }
+  });
+});
