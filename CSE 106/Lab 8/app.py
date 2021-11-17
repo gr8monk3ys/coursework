ce3d19eb1b0fee0afdd0f37d66480081ccb822ec
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-11-16 20:27:58 -0800

    finished login

--- CSE 106/Lab 8/app.py
+++ CSE 106/Lab 8/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, flash, url_for, redirect, render_template, session
+from flask_login import UserMixin, LoginManager, current_user, login_user, logout_user, login_required
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin, AdminIndexView
-from flask_login import UserMixin, LoginManager, current_user, login_user, logout_user
+from flask_bcrypt import Bcrypt
 import pandas as pd
 import numpy as np
 
@@ -20,7 +21,7 @@ login = LoginManager(app)
 
 @login.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return User.query.get(int(user_id))
 
 class Student(db.Model):
     __tablename__ = 'student'
@@ -36,13 +37,15 @@ class Student(db.Model):
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column('id', db.Integer, primary_key=True)
-    username = db.Column('username', db.String(20))
-    password = db.Column('password', db.String(20))
+    username = db.Column('username', db.String(20), nullable=False)
+    password = db.Column('password', db.String(20), nullable=False)
+    type_person = db.Column('type_person', db.Integer())
 
-    def __init__(self, id, username, password):
+    def __init__(self, id, username, password, type_person):
         self.id = id
         self.username = username
         self.password = password
+        self.type_person = type_person
 
 class Teacher(db.Model):
     __tablename__ = 'teacher'
@@ -106,23 +109,22 @@ admin.add_view(ModelView(Class, db.session))
 @app.route("/login", methods=['POST', 'GET'])
 def login():
     """Login the current user."""
-    user = User.query.get(1)
-    login_user(user)
-    return 'Logged in!'
-    # if request.method == 'POST':
-    #     username = request.form.get('username')
-    #     password = request.form.get('password')
-    #     if username == user['username'] and password == password['password']:
-    #         session['user'] = username
-    #         return redirect(url_for('/student'))
-    #     if request.method == 'POST':
-    #         username = request.form.get('username')
-    #         password = request.form.get('password')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-    #     else:
-    #         return "<h1>Wrong username or password</h1>"
-    # else:
-    #     return render_template("login.html")
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if user.type_person == 0:
+                login_user(user)
+                return redirect(url_for('student_view'))
+            elif user.type_person == 1:
+                login_user(user)
+                return redirect(url_for('teacher_view'))
+        else:
+            return "Incorrect username or password"
+    else:
+        return render_template("login.html")
 
 @app.route('/logout')
 def logout():
@@ -130,11 +132,13 @@ def logout():
     logout_user()
     return 'Logged out'
 
-@app.route("/student-view/", methods=['GET', 'POST'])
+@app.route("/student-view", methods=['GET', 'POST'])
+@login_required
 def student_view():
     return render_template('student_view.html')
 
 @app.route("/teacher-view", methods=['GET', 'POST', 'PUT'])
+@login_required
 def teacher_view():
     """Allows teachers to view their current courses and general logistics"""
     return render_template('teacher_view.html')
@@ -158,9 +162,16 @@ def build_df():
 
 if __name__ == "__main__":
 
+    # from app import User, db
     # db.create_all()
     # me = User(id=1, username="Lorenzo", password="hello") 
     # db.session.add(me)
     # db.session.commit()
     # df = build_df()
+
+    # from app import User, db
+    # db.drop_all()
+    # db.create_all()
+    # teacher = User(id=1, username="Ralph Jenkins", password="hi", type_person=1) 
+
     app.run(debug = True)
