89cb404cd3edb15f66fd0859331bc421d546e2e7
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-12-03 07:50:26 -0800

    started question 1

--- CSE 111/Lab 10/test/1.sql
+++ CSE 111/Lab 10/test/1.sql
@@ -1,9 +1,9 @@
-create trigger t1 update of orders on o_orderdate
-begin 
+create trigger t1 before update or insert or update on orders
+for each row 
+when (o_orderdate is '2021-12-01')
+begin
+insert into orders values(
 update orders set o_orderdate =  where customer_name = old.name;
-end
-
-insert into orders
-
 select o_orderdate from orders
-where o_orderdate
+where strftime('%Y', o_orderdate) > '2021'
+end
