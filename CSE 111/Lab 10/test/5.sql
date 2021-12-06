271b475e2074c4f91395ba9ef5bdb789d5f53ede
gr8monk3ys <lorenzosca7@protonmail.ch>
2021-12-05 23:48:33 -0800

    finished question 5

--- CSE 111/Lab 10/test/5.sql
+++ CSE 111/Lab 10/test/5.sql
@@ -1,1 +1,26 @@
-create trigger t5 after update on orders
+drop trigger if exists t5;
+
+create trigger t5 after delete on part
+begin
+delete from lineitem
+where l_partkey = old.p_partkey;
+delete from partsupp
+where ps_partkey = old.p_partkey;
+end;
+
+delete from part
+where p_partkey in(select ps_partkey from partsupp 
+inner join supplier on s_suppkey = ps_suppkey
+inner join nation on n_nationkey = s_nationkey
+where (n_name = 'UNITED STATES' or n_name = 'CANADA'));
+
+select n_name, count(*) from partsupp, region
+inner join part on p_partkey = ps_partkey
+inner join supplier on s_suppkey = ps_suppkey
+inner join nation on n_regionkey = r_regionkey
+where p_partkey = ps_partkey
+and ps_suppkey = s_suppkey
+and s_nationkey = n_nationkey
+and n_regionkey = r_regionkey
+and r_name = 'AMERICA'
+group by n_name;
