b30473b8d97502c2c65ab94f5eebeeb46dd6153e
gr8monk3ys <lorenzosca7@protonmail.ch>
2021-12-05 22:14:15 -0800

    finished question 3

--- CSE 111/Lab 10/test/3.sql
+++ CSE 111/Lab 10/test/3.sql
@@ -1,23 +1,18 @@
-create trigger t3 on customer after update as
+drop trigger if exists t3;
+
+create trigger t3 after update on customer
 begin
-   -- set NOCOUNT ON added to prevent extra result sets from
-   -- interfering with SELECT statements.
-   set NOCOUNT on;
-   declare @new_acctbal DECIMAL(7,2)
-   select @new_acctbal is c_acctbal FROM inserted
+    update customer
+    set c_comment = "Positive balance"
+    where NEW.c_acctbal > 0
+    and OLD.c_acctbal < 0;
+end;
 
-   if @new_acctbal >= 100
-   begin
-       update Customer set c_comment = NULL
-       where c_custkey = (SELECT c_custkey FROM inserted)
-   end
-end
-go
+update customer set c_acctbal = 100
+where c_nationkey in (select n_nationkey from nation
+where n_name = 'UNITED STATES');
 
-update customer set customer.c_acctbal is 100 from customer 
-inner join nation on nation.n_nationkey = customer.c_nationkey
-where nation.n_name is 'ROMANIA'
-
-select * from customer
-inner join nation on nation.n_nationkey is customer.c_nationkey
-where nation.n_name is 'EUROPE' and customer.c_acctbal < 0
+select count(c_custkey) from region
+inner join customer on c_nationkey = n_nationkey
+inner join nation on n_regionkey = r_regionkey
+where c_acctbal < 0 and r_name = 'AMERICA';
