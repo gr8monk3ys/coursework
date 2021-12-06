d829ecadef246f32e9473c3555e6fa4d61600f3a
gr8monk3ys <lorenzosca7@protonmail.ch>
2021-12-05 22:14:07 -0800

    finished question 2

--- CSE 111/Lab 10/test/2.sql
+++ CSE 111/Lab 10/test/2.sql
@@ -1,17 +1,12 @@
-create trigger negative_balance before delete or insert or update on customer
-for each row 
-when (c_acctbal < 0)
+create trigger t2 after update on customer
 begin
-update customer set comment = 'Negative balance !!! Add money now';
-end
+    update customer set c_comment = "Negative balance!!!"
+    where NEW.c_acctbal < 0 and OLD.c_acctbal > 0;
+end;
 
-create trigger positive_balance before delete or insert or update on customer
-for each row 
-when (c_acctbal > 0)
-begin
-update customer set comment = ' ';
-update customer set c_acctbal = -200 where continent='ASIA';
-select * from customer where c_acctbal<0 AND country='CHINA';
-update customer set c_acctbal = 100 where country='CHINA';
-select * from customer where c_acctbal<0 AND continent='ASIA';
-end
+update customer set c_acctbal = -100
+where c_nationkey in (select n_nationkey from nation, region
+where n_regionkey = r_regionkey and r_name = 'AMERICA');
+
+select count(c_custkey) from customer, nation
+where c_acctbal < 0 and c_nationkey = n_nationkey and n_name = 'CANADA';
