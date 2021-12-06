a470eddfefe2fae8b9f723584b265ecaab6dcf0b
gr8monk3ys <lorenzosca7@protonmail.ch>
2021-12-05 22:14:00 -0800

    finished question 1

--- CSE 111/Lab 10/test/1.sql
+++ CSE 111/Lab 10/test/1.sql
@@ -1,9 +1,22 @@
-create trigger t1 before update or insert or delete on orders
-for each row 
-when (o_orderdate is '2021-12-01')
+drop trigger if exists t1;
+
+create trigger t1 after insert on orders
 begin
-insert into orders values(
-update orders set o_orderdate =  where customer_name = old.name;
-select o_orderdate from orders
-where strftime('%Y', o_orderdate) > '2021'
-end
+    update orders
+    set o_orderdate = '2021-12-01'
+    where o_orderkey = NEW.o_orderkey;
+end;
+
+insert into orders 
+select
+    orders.o_orderkey + sub_query.max_id,
+    o_custkey, o_orderstatus, o_totalprice, o_orderdate,
+    o_orderpriority, o_clerk, o_shippriority, o_comment
+from 
+    orders, 
+    (select max(o_orderkey) as max_id from orders) as sub_query
+where substr(o_orderdate, 1, 7) between '1996-12' and '1996-12';
+
+select count(*)
+from orders
+where o_orderdate like '%2021%';
